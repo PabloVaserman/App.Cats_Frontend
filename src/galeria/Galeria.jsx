c44e9a0c160568d20a5296e83d272bc4015d9f4f
Pablo Vaserman <pablovaser@yahoo.com.ar>
2024-05-16 18:41:18 -0300

    Se corrige la sintaxis de la etiqueta Link

--- src/galeria/Galeria.jsx
+++ src/galeria/Galeria.jsx
@@ -1,7 +1,7 @@
 "use client"
 
-import React from "react";
-import { useState } from "react";
+import React,{useEffect,useState} from "react";
+
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
