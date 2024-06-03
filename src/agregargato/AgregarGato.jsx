8804d8ff7f391b396e3e7996d08ca2fd6219c4a3
Pablo Vaserman <pablovaser@yahoo.com.ar>
2024-06-03 19:45:28 -0300

    Se trabaja en el estilo del formulario

--- src/agregargato/AgregarGato.jsx
+++ src/agregargato/AgregarGato.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import axios from "axios";
@@ -28,25 +28,30 @@ function AgregarGato() {
     // CONEXIÓN FRONT/BACK
 
     axios
-      .post("http://localhost:5050/postear/", newCat) // Le pide al endpoint del back que entre a la DB para crear un nuevo gato.
+      .post("http://localhost:5050/rutasGatos/postear/", newCat) // Le pide al endpoint del back que entre a la DB para crear un nuevo gato.
 
       //Cuando consigue la info., entonces...
       .then((res) => console.log(res.data)) // Si encontró la info. la muestra en consola.
       .then((err) => console.log(err)); // Si no la encontró, muestra el error.
     alert("Gato agregado a la DB");
-    navegar(0);
+    navegar("/");
   }
 
   return (
     <div className="w-full bg-stone-500">
-    <div className="container bg-cyan-700">
-      <div className="font-sans text-xl">
-        <h2>Agregar un GATO</h2>
-      </div>
+      <div className="bg-cyan-700 text-center">
+        <div className="font-sans text-3xl mt-4">
+          <h3
+            style={{ fontSize: "2rem", color: "blue", fontWeight: "bold", lineHeight: "4.5"}}
+            className="text-indigo-800"
+          >
+            Agregar un GATO
+          </h3>
+        </div>
 
-      <div className="text-lg font-sans text-indigo-800">
-        <div className="   ">
-          <div className="mb-3">
+        <div className="text-lg font-sans font-bold text-indigo-800 mx-auto mt-4
+        ">
+          <div>
             <label htmlFor="nombre">Nombre</label>
             <input
               type="text"
@@ -59,7 +64,7 @@ function AgregarGato() {
             <label htmlFor="raza">Raza</label>
             <input
               type="text"
-              className="form-control"
+              className="form-control border border-gray-300 rounded-md px-3 py-2"
               value={raza}
               onChange={(e) => setRaza(e.target.value)}
             />
@@ -68,7 +73,7 @@ function AgregarGato() {
             <label htmlFor="peso">Peso</label>
             <input
               type="number"
-              className="form-control"
+              className="form-control border border-gray-300 rounded-md px-3 py-2"
               value={peso}
               onChange={(e) => setPeso(e.target.value)}
             />
@@ -78,40 +83,43 @@ function AgregarGato() {
             <label htmlFor="temperamento">Temperamento</label>
             <input
               type="text"
-              className="form-control"
+              className="form-control border border-gray-300 rounded-md px-3 py-2"
               value={temperamento}
               onChange={(e) => setTemperamento(e.target.value)}
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="peso">Peso</label>
+            <label htmlFor="descripción">Descripción</label>
             <input
-              type="number"
-              className="form-control"
+              type="text"
+              className="form-control border border-gray-300 rounded-md px-3 py-2"
               value={descripcion}
               onChange={(e) => setDescripcion(e.target.value)}
             />
           </div>
 
           <div className="mb-3">
-            <label htmlFor="temperamento">Imagen</label>
+            <label htmlFor="imagen">Imagen</label>
             <input
               type="text"
-              className="form-control"
+              className="form-control border border-gray-300 rounded-md px-3 py-2"
               value={imagen}
+              placeholder="Ingresa la URL"
               onChange={(e) => setImagen(e.target.value)}
             />
           </div>
-
-          <button onClick={postearGato} className="border-2 rounded border-gray-500 p-1 px-2 mr-4 hover:bg-green-800 text-lg transition-all ease-in-out">  {/* Cuando se hace click dispara la función "postearGato" */}
-            Agregar Gato  
-          </button>  
         </div>
+        <button
+          onClick={postearGato}
+          className="border-2 rounded border-gray-500 p-1 px-2 mr-4 mt-6 hover:bg-green-800 text-lg transition-all ease-in-out"
+        >
+          {" "}
+          {/* Cuando se hace click dispara la función "postearGato" */}
+          Agregar Gato
+        </button>
       </div>
     </div>
-
-  </div>
   );
 }
 
-export default AgregarGato;  //
+export default AgregarGato; //
